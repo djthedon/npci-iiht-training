30981439e2f7220a34b51be651949b94a9142bb7
Dibyajyoti Sahoo <35306932+djthedon@users.noreply.github.com>
2021-09-27 12:49:46 +0530

    Tower of Hanoi

---
+++ python_assignments/Assignment2.py
@@ -0,0 +1,19 @@
+a = int(input("Enter the range"))
+b = int(input("Enter the value"))
+
+p = 1
+
+arr = list()
+
+for i in range(1, a + 1):
+    k = 0
+    while k < a  - i:
+        print(end = " ")
+        k += 1
+    for j in range(i):
+        print(p, end = " ")
+    arr.append(i * p)
+    p += b
+    print()
+
+print('\n', arr)
