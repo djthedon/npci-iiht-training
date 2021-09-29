bc5a01c77fa4ab7feaba11466943cfb5a143840b
Dibyajyoti Sahoo <35306932+djthedon@users.noreply.github.com>
2021-09-29 11:44:23 +0530

    Lambda.py

---
+++ python_assignments/Day_3/Assignment1.py
@@ -0,0 +1,19 @@
+# 1
+l = []
+i = int(input("Enter the number of elements in the user"))
+
+input_lis = lambda x : [int(input()) for j in range(x)]
+
+b = input_lis(i)
+
+print(b)
+
+# 2
+
+input_dic_list = lambda x : {i : i + 1 for i in x}
+
+c = input_dic_list(b)
+
+print(c)
+
+# 3
