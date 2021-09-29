7999569386deefac3dd4799b0542e9d7a8b9c0dc
Dibyajyoti Sahoo <35306932+djthedon@users.noreply.github.com>
2021-09-29 16:30:09 +0530

    Assignemnt3.py

--- python_assignments/Day_3/Assignment3.py
+++ python_assignments/Day_3/Assignment3.py
@@ -1,18 +1,21 @@
 a = [
-        ['Mohsin', 'mn@gg.com', '1283'],
+        ['Mohsin', 'mn@gg.com', '1283', 'city', 'street'],
         ['Test', 'tt@user.com', '9999'],
         ['Username', 'email', '12478'],
-        ['Hello', 'email@34','9456']
+        ['Hello', 'email@34','9456'],
+        [],
+        ['pqr', 'rty', 'zxc']
     ]
 
 with open('sample.txt', 'w') as r:
     r.write("name,email,phone \n")
     for i in a:
+        if len(i) <= 2:
+            continue
         p = 0
-        while p < len(i) - 1:
+        while p < 2:
             r.write(i[p])
             r.write(',')
             p += 1
         r.write(i[p])
         r.write('\n')
-
